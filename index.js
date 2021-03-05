88b14b72692c82d10998616fe733b5982e01394c
kxc3244 <kevin.chawla@mavs.uta.edu>
2021-03-04 22:18:57 -0600

    /record API GET

--- index.js
+++ index.js
@@ -6,26 +6,28 @@ const app = express()
 const port = process.env.PORT || 4000;
 app.use(cors())
 
-
-app.get('/', (req, res) => {
-  res.send(data)
+const uuidData = data.map((i)=>{
+  return {
+     ...i,
+     id: uuidv4()
+  }
 })
-app.get('/allrecords', (req, res) => {
-    const uuidData = data.map((i)=>{
-        return {
-           ...i,
-           id: uuidv4()
-        }
-     })
 
+app.get('/records',(req,res)=>{
+  const id = req.query.id;
+  const record = uuidData.filter(i => i.id === id);
+  res.send(record)
+})
+
+
+app.get('/allrecords', (req, res) => {    
      res.send(uuidData)
 })
 
 
-app.get('/allrecord',(req,res)=>{
-  const id = req.query.id;
-  const record = uuidData.filter(i => i.id === id);
-  res.send(record)
+
+app.get('/', (req, res) => {
+  res.send(data)
 })
 
 
